7f74cd3613a6ceccad4db254abbf1846cb8fce69
wuqi-jack <62972891+wuqi-jack@users.noreply.github.com>
2020-07-10 10:35:10 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -1,3 +1,28 @@
 {
-    "keyLength": 64
+  "keyLength": 2048,
+  "io": false,
+  "webUrl": "https://update-0001.17itou.com/hot_code_com.wanglizhi.clientd_dist/koudai/",
+  "thirdPartUrlPrefix": [
+    "https://cash.yeepay.com",
+    "https://m.51yueqian.com",
+    "https://wap.beeplaying.com",
+    "https://h5.jdddata.com/communal/cartoonKou/fulllive",
+    "http://cash.yeepay.com",
+    "http://m.51yueqian.com",
+    "http://wap.beeplaying.com",
+    "http://h5.jdddata.com/communal/cartoonKou/fulllive"
+  ],
+  "schemeLaunchUrl": [
+    "weixin://",
+    "sms://",
+    "tel://",
+    "alipays://",
+    "mailto:"
+  ],
+  "syncCookiesUrl": "https://www.17itou.com",
+  "hotCodeUrl": "https://update-0001.17itou.com/hot_code_com.wanglizhi.clientd_dist/koudai/",
+  "hotCodeZipUrl": "https://update-0001.17itou.com/hot_code_com.wanglizhi.clientd_dist/koudai_zip/",
+  "enableHotCode": true,
+  "enableHotCodeZip": true,
+  "enableBlue": false
 }
